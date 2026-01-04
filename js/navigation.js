305436316bc78a70b0bdd87935b790340c086ead
JEFERSON BARBOSA DOS SANTOS <73561253+Jeferson29@users.noreply.github.com>
2026-01-04 13:01:46 -0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,6 @@
+"use strict";
+
 let currentSection = null;
-
-const form = document.querySelector(".contact-form");
-
-if (form) {
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    alert("Mensagem enviada! (simulação)");
-  });
-}
 
 document.querySelector(".about-cta")
   .addEventListener("click", () => openSection("projects"));
